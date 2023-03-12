7e170a9b0ce75437c049ae9a12de021bddcf8ab5
IIIAKO <iiiako197@gmail.com>
2023-03-12 18:23:36 +0200

    Created the main views based on MVVM pattern

---
+++ KeyboardTrainerWPF/MVVM/ViewModels/MainViewModel.cs
@@ -0,0 +1,65 @@
+﻿using KeyboardTrainerWPF.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyboardTrainerWPF.MVVM.ViewModels
+{
+    internal class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand RecordsTableViewCommand { get; set; }
+        public RelayCommand TrainViewCommand { get; set; }
+        public RelayCommand SettingsViewCommand { get; set; }
+
+
+        public HomeViewModel HomeVM { get; set; }
+        public RecordsTableViewModel RecordsTableVM { get; set; }
+        public TrainViewModel TrainVM { get; set; }
+        public SettingsViewModel SettingsVM { get; set; }
+
+
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            RecordsTableVM = new RecordsTableViewModel();
+            TrainVM= new TrainViewModel();
+            SettingsVM = new SettingsViewModel();
+
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(x =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            RecordsTableViewCommand = new RelayCommand(x => 
+            { 
+                CurrentView = RecordsTableVM;
+            });
+
+            TrainViewCommand = new RelayCommand(x =>
+            {
+                CurrentView = TrainVM;
+            });
+
+            SettingsViewCommand = new RelayCommand(x =>
+            {
+                CurrentView = SettingsVM;
+            });
+        }
+    }
+}
