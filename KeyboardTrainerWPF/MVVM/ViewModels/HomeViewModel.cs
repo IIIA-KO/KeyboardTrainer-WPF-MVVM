0c65595cc8967128ae64ee638b32039eb5a92525
IIIAKO <iiiako197@gmail.com>
2023-05-07 13:38:49 +0300

    Connection string fix #2

--- KeyboardTrainerWPF/MVVM/ViewModels/HomeViewModel.cs
+++ KeyboardTrainerWPF/MVVM/ViewModels/HomeViewModel.cs
@@ -278,7 +278,6 @@ namespace KeyboardTrainerWPF.MVVM.ViewModels
 
         private void Stop()
         {
-            //_speedTracker.Stop();
             _progressBar.Value = 0;
             Speed = 0;
             Fails = 0;
